28ce908ef95164d608f267a116707d0e44a00538
Adam Pilato <adam.pilato@smartech-aust.com>
2023-01-17 19:54:34 +1100

    added api call function and comments

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var citySearch = document.querySelector('#search-city');
 var displayDate= moment();
 var searchHistory = [];
 
-
+//setting up local storage based on search history
 function search(town) {
     forecast.innerHTML = "";
     current.innerHTML = "";
@@ -17,5 +17,16 @@ function search(town) {
     if (!searchHistory.includes(town)) {
     searchHistory.push(town);
     localStorage.setItem("weatherSearch", JSON.stringify(searchHistory));
+    }
+}
+//fecthing the co-ordinates from the localised search 
+function coordinates(town){
+    let weatherCall = "api.openweathermap.org/data/2.5/forecast?q="+town+" &appid" + APIkey;
+
+    for(weatherCall){
+        fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid"+ APIkey);
+        .then((response) => response.json());
+        .then((data) => console.log(data));
+
     }
 }
