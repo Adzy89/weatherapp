423010f9c0123b469e20bd239e8894f5b1ebc8ec
adzzy89 <adam_pilato@hotmail.com>
2023-01-28 16:55:21 +1100

    added fetchlonglat, fetchweather and current weather functions

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ var citySearch = document.querySelector('#search-city');
 var displayDate= moment();
 var searchHistory = [];
 
-//setting up local storage based on search history
 function search(town) {
     forecast.innerHTML = "";
     current.innerHTML = "";
@@ -18,15 +17,67 @@ function search(town) {
     searchHistory.push(town);
     localStorage.setItem("weatherSearch", JSON.stringify(searchHistory));
     }
+    render();
+    fetchlongLat(town)
 }
-//fecthing the co-ordinates from the localised search 
-function coordinates(town){
-    let weatherCall = "api.openweathermap.org/data/2.5/forecast?q="+town+" &appid" + APIkey;
 
-    for(weatherCall){
-        fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid"+ APIkey);
-        .then((response) => response.json());
-        .then((data) => console.log(data));
 
+async function fetchlongLat(town) {
+    try {
+    const response = await fetch("api.openweathermap.org/data/2.5/forecast?q="+town+" &appid" + APIkey;);
+    const data = await response.json();
+    const lat = data.city.coord.lat;
+    const lon = data.city.coord.lon;
+    const cityName = data.city.name;
+    fetchWeather(lat, lon, cityName);
+    } catch (error) {
+        console.error(error);
     }
 }
+
+async function fetchWeather(lat, lon, APIkey) {
+    try {
+      const weatherCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,alerts&appid=${APIkey}&units=imperial`;
+      const response = await fetch(weatherCall);
+      const data = await response.json();
+      currentWeather(data);
+      fiveDayWeather(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  function currentWeather(data) {
+    const cityName = document.createElement('h3');
+    cityName.classList.add('row');
+    cityName.textContent = `${searchedCity} (${displayDate.format("DD/MM/YYYY")})`;
+    
+    const img = document.createElement('img');
+    const icon = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`;
+    img.setAttribute("src", icon);
+  
+    const conditions = document.createElement('ul');
+    conditions.classList.add('column');
+  
+    const temp = document.createElement('li');
+    temp.textContent = `Temp: ${data.current.temp} \xB0F`;
+  
+    const wind = document.createElement('li');
+    wind.textContent = `Wind: ${data.current.wind_speed} MPH`;
+  
+    const humidity = document.createElement('li');
+    humidity.textContent = `Humidity: ${data.current.humidity} %`;
+  
+    const UV = document.createElement('li');
+    UV.textContent = `UV Index: ${data.current.uvi}`;
+    UvColorCoder(data.current.uvi, UV);
+  
+    current.appendChild(cityName);
+    current.appendChild(img);
+    current.appendChild(conditions);
+    conditions.appendChild(temp);
+    conditions.appendChild(wind);
+    conditions.appendChild(humidity);
+    conditions.appendChild(UV);
+  }
+  
